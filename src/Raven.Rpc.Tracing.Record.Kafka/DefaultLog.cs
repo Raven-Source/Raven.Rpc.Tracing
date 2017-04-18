6cbcb990c663ecbbd1054c04c5f1c05f8ea3f867
indifer <indifer@126.com>
2017-04-18 17:23:14 +0800

    update raven.rpc.tracing.record.kafka

---
+++ src/Raven.Rpc.Tracing.Record.Kafka/DefaultLog.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raven.Rpc.Tracing.Record
+{
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    public class DefaultLog : Message.Kafka.Abstract.ILog
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="format"></param>
+        /// <param name="pars"></param>
+        public void Debug(string format, params object[] pars)
+        {
+            
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="ex"></param>
+        public void Error(Exception ex)
+        {
+            
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="format"></param>
+        /// <param name="pars"></param>
+        public void Error(string format, params object[] pars)
+        {
+            
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="format"></param>
+        /// <param name="pars"></param>
+        public void Info(string format, params object[] pars)
+        {
+            
+        }
+    }
+}
