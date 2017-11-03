254e7412b07e4d3d371cbc6303979f96dbe66248
liangyi <liangyi@mallcoo.cn>
2017-11-03 18:26:47 +0800

    create

---
+++ src/Test/Raven.AspNetCore.WebApiExtensions.Tracing.TestConsole/Controllers/TestController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Raven.AspNetCore.WebApiExtensions.Tracing.TestConsole.Controllers
+{
+    [Route("api/[controller]")]
+    public class TestController : Controller
+    {
+        [HttpGet]
+        [CustemFilter]
+        public User Get()
+        {
+            return new Controllers.User() {  Name = "sdggas"};
+        }
+
+    }
+
+    public class User
+    {
+        public long ID
+        {
+            get; set;
+        }
+
+        public string Name { get; set; }
+
+        public static int a;
+
+        public string Desc { get; set; }
+
+        public virtual void Say()
+        {
+        }
+    }
+
+    public class GoodUser
+    {
+    }
+
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true, Inherited = true)]
+    public class CustemFilterAttribute : Microsoft.AspNetCore.Mvc.Filters.ActionFilterAttribute
+    {
+        
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            base.OnActionExecuting(context);
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            base.OnActionExecuted(context);
+        }
+
+
+    }
+
+}
---
+++ src/Test/Raven.AspNetCore.WebApiExtensions.Tracing.TestConsole/Program.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Raven.AspNetCore.WebApiExtensions.Tracing.TestConsole
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            return WebHost.CreateDefaultBuilder(args)
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .Build();
+        }
+
+    }
+
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            // 注入MVC框架
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            // 添加MVC中间件
+            app.UseMvc();
+
+            //app.Run(context =>
+            //{
+            //    return context.Response.WriteAsync("Hello from ASP.NET Core!");
+            //});
+        }
+    }
+
+}
